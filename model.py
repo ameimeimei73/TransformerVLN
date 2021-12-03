3b67a04809466bcc436f3eebfdd25bcca7947606
Mei <chenmeili070308@gmail.com>
2021-12-02 22:37:41 -0500

    modify model

--- model.py
+++ model.py
@@ -3,15 +3,15 @@ import torch.nn as nn
 from transformers import T5Model
 
 class CustomT5Model(nn.Module):
-    def __init__(self, input_action_size, output_action_size, image_feature_size):
+    def __init__(self, action_size, image_feature_size):
         super(CustomT5Model, self).__init__()
 
-        self.num_labels = output_action_size
+        self.num_labels = action_size
         self.base_model = T5Model.from_pretrained('t5-small')  # small for test, should change to base
 
         # Change beam search to greedy search
         self.base_model.config.num_beams = 1
-        self.decoder_input = nn.Linear(in_features=input_action_size + image_feature_size, out_features=512)
+        self.decoder_input = nn.Linear(in_features=action_size + image_feature_size, out_features=512)
         self.dense = nn.Linear(in_features=512, out_features=self.num_labels)
 
     def forward(self, input_ids, attn_mask, actions, image_features):
@@ -28,17 +28,19 @@ class CustomT5Model(nn.Module):
 
         hidden_states = output['last_hidden_state']
         logits = self.dense(hidden_states)
+        probs = torch.nn.functional.softmax(logits, dim=2)
+        predictions = torch.argmax(probs, dim=2)
 
-        return logits
+        return predictions
 
 def main():
-    model = CustomT5Model(8, 6, 1024)
+    model = CustomT5Model(8, 1024)
     input_ids = torch.LongTensor([[45, 32, 456, 34, 8, 57]])
     attention_mask = torch.LongTensor([[1, 1, 1, 1, 1, 1]])
     actions = torch.rand([1, 10, 8])
     image_features = torch.rand([1, 10, 1024])
     output = model(input_ids=input_ids, attn_mask=attention_mask, actions=actions, image_features=image_features)
-    print(type(output), output.shape)
+    print(output, output.shape)
 
 
 if __name__ == "__main__":
