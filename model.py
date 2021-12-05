be00001a072cb1b96c00f7fa3fe1b76dfe6f4ab3
EEEEEEYu <elvisyuovo@gmail.com>
2021-12-04 23:16:31 -0500

    fix bug

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
+from torch.nn.functional import one_hot
 from transformers import T5Model
+
 
 class CustomT5Model(nn.Module):
     def __init__(self, action_size, image_feature_size):
@@ -9,6 +11,16 @@ class CustomT5Model(nn.Module):
         self.num_labels = action_size
         self.base_model = T5Model.from_pretrained('t5-small')  # small for test, should change to base
         self.model_actions = ['left', 'right', 'up', 'down', 'forward', '<end>', '<start>', '<ignore>']
+        self.env_actions = [
+            (0, -1, 0),  # left
+            (0, 1, 0),  # right
+            (0, 0, 1),  # up
+            (0, 0, -1),  # down
+            (1, 0, 0),  # forward
+            (0, 0, 0),  # <end>
+            (0, 0, 0),  # <start>
+            (0, 0, 0)  # <ignore>
+        ]
 
         # Change beam search to greedy search
         self.base_model.config.num_beams = 1
@@ -36,28 +48,32 @@ class CustomT5Model(nn.Module):
 
     def test(self, env, instruction_ids, max_steps):
         env.reset()
-        action = torch.LongTensor(self.model_actions.index('<start>'))
-        img_feature = env.step(action)
+        action = one_hot(torch.LongTensor([[6]]), num_classes = 8)
+        obs = env.step((0, 0, 0))
+        img_feature = torch.tensor(obs['features']).float()
         decoder_input = torch.cat((action, img_feature), 2)
         attention_mask = torch.ones_like(instruction_ids)
+        decoder_embeds = self.decoder_input(decoder_input)
 
         cur_step = 0
         output_action_seq = [action]
         # stop until reaches max step limits or model outputs <end> action
-        while cur_step < max_steps and action is not self.model_actions.index('<end>'):
+        while cur_step < max_steps and action.cpu().item() is not self.model_actions.index('<end>'):
             output = self.base_model(
                 instruction_ids,
                 attention_mask=attention_mask,
                 output_hidden_states=True,
-                decoder_inputs_embeds=decoder_input
+                decoder_inputs_embeds=decoder_embeds
             )
 
             hidden_states = output['last_hidden_state']
             action_prob = self.dense(hidden_states).softmax(dim=2)
             action = action_prob.argmax(dim=2)
-            img_feature = env.step(action)
-            next_input = torch.cat((action, img_feature), dim=2)
+            obs = env.step(self.env_actions[action.squeeze().cpu().item()])
+            img_feature = torch.tensor(obs['features']).float()
+            next_input = torch.cat((one_hot(action, num_classes = 8), img_feature), dim=2)
             decoder_input = torch.cat((decoder_input, next_input), dim=1)
+            decoder_embeds = self.decoder_input(decoder_input)
 
             output_action_seq.append(action)
             cur_step += 1
@@ -73,6 +89,9 @@ def main():
     image_features = torch.rand([1, 10, 1024])
     output = model(input_ids=input_ids, attn_mask=attention_mask, actions=actions, image_features=image_features)
     print(output, output.shape)
+
+    a = torch.tensor([[6]])
+    print(one_hot(a, num_classes = 8))
 
 
 if __name__ == "__main__":
